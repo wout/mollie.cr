0706afc4e9820b2c70bbf227130560cb15cbc1af
wout <wout@mick-wout.com>
2020-03-02 18:14:46 +0000

    Added refunded? method and application fee to Payment.

--- spec/mollie/payment_spec.cr
+++ spec/mollie/payment_spec.cr
@@ -35,18 +35,22 @@ describe Mollie::Payment do
       payment.authorized_at.should eq(Time.parse_rfc3339("2018-03-19T09:14:37+00:00"))
       payment.paid_at.should eq(Time.parse_rfc3339("2018-03-20T09:14:37+00:00"))
       payment.amount.should be_a(Mollie::Amount)
+      payment.amount_captured.should be_a(Mollie::Amount?)
+      payment.amount_refunded.should be_a(Mollie::Amount?)
+      payment.amount_remaining.should be_a(Mollie::Amount?)
       payment.description.should eq("My first payment")
       payment.method.should eq("ideal")
       payment.metadata.should be_a(HSBFIS)
       payment.metadata["order_id"].should eq("12345")
-      payment.details.should be_a(HSBFIS)
-      payment.details["consumer_name"].should eq("Hr E G H K\u00fcppers en\/of MW M.J. K\u00fcppers-Veeneman")
-      payment.details["consumer_account"].should eq("NL53INGB0618365937")
-      payment.details["consumer_bic"].should eq("INGBNL2A")
+      details = payment.details.as(HSBFIS)
+      details["consumer_name"].should eq("Hr E G H K\u00fcppers en\/of MW M.J. K\u00fcppers-Veeneman")
+      details["consumer_account"].should eq("NL53INGB0618365937")
+      details["consumer_bic"].should eq("INGBNL2A")
       payment.locale.should eq("nl_NL")
       payment.country_code.should eq("NL")
       payment.profile_id.should eq("pfl_QkEhN94Ba")
       payment.webhook_url.should eq("https://webshop.example.org/payments/webhook")
+      payment.application_fee.should be_a(Mollie::Payment::ApplicationFee?)
     end
   end
 
@@ -63,10 +67,66 @@ describe Mollie::Payment do
     end
   end
 
+  describe "#refunded?" do
+    it "tests positive with a refunded amount" do
+      payment = Mollie::Payment.from_json(get_refunded_payment_json)
+      payment.refunded?.should be_true
+    end
+    it "tests negative with a refunded amount of zero" do
+      payment = Mollie::Payment.from_json(get_zero_refunded_payment_json)
+      payment.refunded?.should be_false
+    end
+    it "tests negative without a refunede amount" do
+      payment = Mollie::Payment.from_json(get_payment_json)
+      payment.refunded?.should be_false
+    end
+  end
+
   describe "#checkout_url" do
     it "fetches the checkout url from links" do
       payment = Mollie::Payment.from_json(get_payment_json)
       payment.checkout_url.should be(payment.links.as(HSHS2)["checkout"]["href"])
+    end
+  end
+
+  describe "#application_fee" do
+    it "parses the application fee as an object" do
+      configure_test_api_key
+      WebMock.stub(:get, "https://api.mollie.com/v2/payments/tr_WDqYK6vllg")
+        .to_return(status: 200, body: get_complete_payment_json)
+
+      payment = Mollie::Payment.get("tr_WDqYK6vllg")
+      fee = payment.application_fee.as(Mollie::Payment::ApplicationFee)
+      fee.amount.value.should eq(42.1)
+      fee.amount.currency.should eq("EUR")
+      fee.description.should eq("Example application fee")
+    end
+  end
+
+  describe "#restrict_payment_methods_to_country" do
+    it "restricts a payment to a country" do
+      configure_test_api_key
+      WebMock.stub(:get, "https://api.mollie.com/v2/payments/tr_WDqYK6vlle")
+        .to_return(status: 200, body: read_fixture("payments/get.json"))
+
+      payment = Mollie::Payment.get("tr_WDqYK6vlle")
+      payment.restrict_payment_methods_to_country.should eq("NL")
+    end
+  end
+
+  describe ".create" do
+    it "creates a payments" do
+      configure_test_api_key
+      body = %({"customerId":"cst_8wmqcHMN4U","amount":{"value":1.95,"currency":"EUR"}})
+      WebMock.stub(:post, "https://api.mollie.com/v2/payments")
+        .with(body: body)
+        .to_return(status: 201, body: read_fixture("payments/post.json"))
+
+      payment = Mollie::Payment.create(data: {
+        customer_id: "cst_8wmqcHMN4U",
+        amount:      {value: 1.95, currency: "EUR"},
+      })
+      payment.customer_id.should eq("cst_8wmqcHMN4U")
     end
   end
 end
--- src/mollie/payment.cr
+++ src/mollie/payment.cr
@@ -20,30 +20,39 @@ struct Mollie
       end
     end
 
-    getter id : String
-    getter mode : String
-    getter description : String
+    getter amount : Mollie::Amount
+    getter amount_captured : Mollie::Amount?
+    getter amount_refunded : Mollie::Amount?
+    getter amount_remaining : Mollie::Amount?
+    @[JSON::Field(key: "applicationFee")]
+    getter application_fee : Mollie::Payment::ApplicationFee?
+    @[JSON::Field(key: "authorizedAt", converter: Mollie::Json::TimeFormatter)]
+    getter authorized_at : Time?
+    @[JSON::Field(key: "countryCode")]
+    getter country_code : String?
     @[JSON::Field(key: "createdAt", converter: Mollie::Json::TimeFormatter)]
     getter created_at : Time
-    getter status : String
-    @[JSON::Field(key: "authorizedAt", converter: Mollie::Json::TimeFormatter)]
-    getter authorized_at : Time
-    @[JSON::Field(key: "paidAt", converter: Mollie::Json::TimeFormatter)]
-    getter paid_at : Time
-    getter amount : Mollie::Amount
-    getter method : String
-    @[JSON::Field(key: "countryCode")]
-    getter country_code : String
-    getter metadata : HSBFIS
+    @[JSON::Field(key: "customerId")]
+    getter customer_id : String?
+    getter description : String
     @[JSON::Field(converter: Mollie::Json::Underscorer)]
-    getter details : HSBFIS
+    getter details : HSBFIS?
+    getter id : String
     getter locale : String
+    getter metadata : HSBFIS
+    getter method : String?
+    getter mode : String
+    @[JSON::Field(key: "paidAt", converter: Mollie::Json::TimeFormatter)]
+    getter paid_at : Time?
     @[JSON::Field(key: "profileId")]
     getter profile_id : String
-    @[JSON::Field(key: "webhookUrl")]
-    getter webhook_url : String
     @[JSON::Field(key: "redirectUrl")]
     getter redirect_url : String
+    @[JSON::Field(key: "restrictPaymentMethodsToCountry")]
+    getter restrict_payment_methods_to_country : String?
+    getter status : String
+    @[JSON::Field(key: "webhookUrl")]
+    getter webhook_url : String
 
     {% for value in Status.constants %}
       {% downcased = value.stringify.downcase %}
@@ -52,8 +61,23 @@ struct Mollie
       end
     {% end %}
 
+    def refunded?
+      if amount_refunded
+        amount_refunded.as(Mollie::Amount).value > 0
+      else
+        false
+      end
+    end
+
     def checkout_url
       Util.extract_url(links, "checkout")
+    end
+
+    struct ApplicationFee
+      include JSON::Serializable
+
+      getter amount : Mollie::Amount
+      getter description : String
     end
   end
 end
