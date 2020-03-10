336322e24779cf3d0b662297fbb4b3685ca08dc9
wout <wout@mick-wout.com>
2020-03-10 16:10:23 +0000

    Implemented permission.

---
+++ src/mollie/permission.cr
@@ -0,0 +1,28 @@
+struct Mollie
+  struct Permission < Base::Resource
+    include Mixins::Linkable
+
+    AVAILABLE = %w[
+      customers.read
+      customers.write
+      invoices.read
+      mandates.read
+      mandates.write
+      organizations.read
+      organizations.write
+      payments.read
+      payments.write
+      profiles.read
+      profiles.write
+      refunds.read
+      refunds.write
+      settlements.read
+      subscriptions.read
+      subscriptions.write
+    ]
+
+    json_field(:description, String)
+    json_field(:granted, Bool)
+    json_field(:id, String)
+  end
+end
